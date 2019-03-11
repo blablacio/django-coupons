08116ef4f8977b5b04f2ad5ae5ec269e83b8f2a2
Marija Milicevic <majce.milicevic@gmail.com>
2019-03-11 08:47:58 +0100

    Add help text for user_limit Coupon field

--- coupons/models.py
+++ coupons/models.py
@@ -78,7 +78,7 @@ class Coupon(models.Model):
         _("Code"), max_length=30, unique=True, blank=True,
         help_text=_("Leaving this field empty will generate a random code."))
     type = models.CharField(_("Type"), max_length=20, choices=COUPON_TYPES)
-    user_limit = models.PositiveIntegerField(_("User limit"), default=1)
+    user_limit = models.PositiveIntegerField(_("User limit"), default=1, help_text=_("Set 0 for unlimited use"))
     created_at = models.DateTimeField(_("Created at"), auto_now_add=True)
     valid_until = models.DateTimeField(
         _("Valid until"), blank=True, null=True,
